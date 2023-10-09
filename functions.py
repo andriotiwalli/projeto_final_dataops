4f1aa2f906c9b406ed762c718ecdff433730a7a5
andriotiwalli <147437500+andriotiwalli@users.noreply.github.com>
2023-10-09 17:06:36 -0300

    incrementando soluções

--- functions.py
+++ functions.py
@@ -1,15 +1,30 @@
-import pandas as pd
+from datetime import datetime
+import os
 
 
-def consulta_api():
-    from api_consulta_https import api_people, api_planets, api_films
+def padronizar_data(data_original):
+    try:
+        data_hora_objeto = datetime.strptime(data_original, '%Y-%m-%dT%H:%M:%S.%fZ')
+        data_formatada = data_hora_objeto.strftime('%d/%m/%Y')
+        return data_formatada
+    except ValueError:
+        return data_original 
+    
+    
+def padronizando_datas(df):
+    for coluna in df.columns:
+        if df[coluna].dtype == 'object':
+            df[coluna] = df[coluna].apply(padronizar_data)
 
-    a = api_people(1)
-    df = pd.DataFrame(a)
+def upload_arquivo(data_atual, df_pessoa, df_planetas, df_filme,path):
+    pessoa_folder_path = f'C:/projeto_final_dataops/lake/{path}/people'
+    planetas_folder_path = f'C:/projeto_final_dataops/lake/{path}/planets'
+    filme_folder_path = f'C:/projeto_final_dataops/lake/{path}/films'
 
+    pessoa_csv_path = os.path.join(pessoa_folder_path, f'pessoas_{data_atual}.csv')
+    planetas_csv_path = os.path.join(planetas_folder_path, f'planetas_{data_atual}.csv')
+    filme_csv_path = os.path.join(filme_folder_path, f'filmes_{data_atual}.csv')
 
-
-    b = api_planets(1)
-    c = api_films(1)
-
-consulta_api()
+    df_pessoa.to_csv(pessoa_csv_path, index=False)
+    df_planetas.to_csv(planetas_csv_path, index=False)
+    df_filme.to_csv(filme_csv_path, index=False)
---
+++ task.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from functions import upload_arquivo
+from datetime import datetime
+
+
+
+
+
+def consulta_api_lake_bronze():
+    from api_consulta_https import api_people, api_planets, api_films
+
+
+    data_atual = datetime.now().strftime('%Y-%m-%d')
+
+    pessoa = api_people(1)
+    df_pessoa = pd.DataFrame([pessoa])
+
+    planetas = api_planets(1)
+    df_planetas = pd.DataFrame([planetas])
+
+    filme = api_films(1)
+    df_filme = pd.DataFrame([filme])
+
+    upload_arquivo(data_atual, df_pessoa, df_planetas, df_filme, 'bronze')
+
+
+
+def bronze_silver_etl():
+    from functions import padronizando_datas
+    
+    data_atual = datetime.now().strftime('%Y-%m-%d')
+
+    pessoa_path = f'C:/projeto_final_dataops/lake/bronze/people/pessoas_{data_atual}.csv'
+    planetas_path = f'C:/projeto_final_dataops/lake/bronze/planets/planetas_{data_atual}.csv'
+    filme_path = f'C:/projeto_final_dataops/lake/bronze/films/filmes_{data_atual}.csv'
+
+    df_pessoa = pd.read_csv(pessoa_path)
+    df_planetas = pd.read_csv(planetas_path)
+    df_filme = pd.read_csv(filme_path)
+    
+    #ETL DATAFRAMES
+
+    # padronizando datas dos datafranes
+    dfs= [df_pessoa,df_planetas,df_filme]
+    for df in dfs:
+        padronizando_datas(df)
+        
+    upload_arquivo(data_atual, df_pessoa, df_planetas, df_filme, 'silver')
+
+
+consulta_api_lake_bronze()
+bronze_silver_etl()
+
+
+
