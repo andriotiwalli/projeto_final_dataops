18a7f5f96f1ff5409243cd62643f61fa2c63426d
andriotiwalli <147437500+andriotiwalli@users.noreply.github.com>
2023-10-20 19:40:07 -0300

    Update functions.py
    Incluido tratamento de erro

--- dags/functions.py
+++ dags/functions.py
@@ -5,63 +5,89 @@ import logging
 import re
 
 def aplicar_alteracoes(df, identificador):
-    mapeamento_colunas = renomear_colunas()
-
-    if identificador == 'filme':
-        df = df.rename(columns=mapeamento_colunas[0]) # nomeando as colunas para Português
-        df = df.applymap(lambda x: '' if x == '[]' else x) # alterando os valores [] para vazio
-        df = remover_duplicados(df, 'numero_do_episodio')  #removendo duplicados
-        df = calcular_porcentagem_nulos(df) # calculando porcentagem de nulos
-
-       
-    elif identificador == 'pessoa':
-        df = df.rename(columns=mapeamento_colunas[1])  # nomeando as colunas para Português
-        df = df.applymap(lambda x: '' if x == '[]' else x) # alterando os valores [] para vazio
-        df = remover_duplicados(df, 'nome') #removendo duplicados
-        df = calcular_porcentagem_nulos(df) # calculando porcentagem de nulos
+    try:
+        mapeamento_colunas = renomear_colunas()
+        if identificador == 'filme':
+            df = df.rename(columns=mapeamento_colunas[0]) # nomeando as colunas para Português
+            df = df.applymap(lambda x: '' if x == '[]' else x) # alterando os valores [] para vazio
+            df = remover_duplicados(df, 'numero_do_episodio')  #removendo duplicados
+            df = calcular_porcentagem_nulos(df) # calculando porcentagem de nulos
 
         
-    elif identificador == 'planetas':
-        df = df.rename(columns=mapeamento_colunas[2])  # nomeando as colunas para Português
-        df = remover_duplicados(df, 'nome') #removendo duplicados
-        df = calcular_porcentagem_nulos(df) # calculando porcentagem de nulos
+        elif identificador == 'pessoa':
+            df = df.rename(columns=mapeamento_colunas[1])  # nomeando as colunas para Português
+            df = df.applymap(lambda x: '' if x == '[]' else x) # alterando os valores [] para vazio
+            df = remover_duplicados(df, 'nome') #removendo duplicados
+            df = calcular_porcentagem_nulos(df) # calculando porcentagem de nulos
+
+            
+        elif identificador == 'planetas':
+            df = df.rename(columns=mapeamento_colunas[2])  # nomeando as colunas para Português
+            df = remover_duplicados(df, 'nome') #removendo duplicados
+            df = calcular_porcentagem_nulos(df) # calculando porcentagem de nulos
+        else:
+            raise ValueError("Identificador inválido. Deve ser 'filme', 'pessoa' ou 'planetas'.")
+    except Exception as e:
+        logging.info(f"Ocorreu um erro em 'aplicar_alteracoes': {str(e)}")
+        return None 
  
     return df
 
 
 def calcular_porcentagem_nulos(df):
-    null_percentages = (df.isnull().sum() / len(df)) * 100
+    try:
+        null_percentages = (df.isnull().sum() / len(df)) * 100
 
-    result_df = pd.DataFrame(null_percentages).T
-    result_df.columns = ['%' + col for col in result_df.columns]
+        result_df = pd.DataFrame(null_percentages).T
+        result_df.columns = ['%' + col for col in result_df.columns]
 
-    df = pd.concat([df, result_df], axis=1)
-
+        df = pd.concat([df, result_df], axis=1)
+    except Exception as e:
+        logging.info(f"Ocorreu um erro em 'calcular_porcentagem_nulos': {str(e)}")
+        return None  
     return df
 
 def remover_caracteres_especiais(df):
-    for coluna in df.columns:
-        df[coluna] = df[coluna].apply(lambda x: re.sub(r'[^\w\s]', '', x))
+    try:
+        for coluna in df.columns:
+            df[coluna] = df[coluna].apply(lambda x: re.sub(r'[^\w\s]', '', x))
+    except Exception as e:
+        logging.info(f"Ocorreu um erro em 'remover_caracteres_especiais': {str(e)}")
+        return None  
     return df
 
 def remover_duplicados(df,colunas):
-  
-    df_sem_duplicatas = df.drop_duplicates(subset=[colunas])
+    try:
+        df_sem_duplicatas = df.drop_duplicates(subset=[colunas])
+    except Exception as e:
+        logging.info(f"Ocorreu um erro em 'remover_duplicados': {str(e)}")
+        return None
     return df_sem_duplicatas
 
 def combinando_arquivos(diretorio):
-    dfs = []
-    for filename in os.listdir(diretorio):
-        if filename.endswith('.csv'):
-            filepath = os.path.join(diretorio, filename)
-            df = pd.read_csv(filepath, sep=";")
-            dfs.append(df)
-    return pd.concat(dfs, ignore_index=True)
-
+   
+    try:
+        dfs = []
+        for filename in os.listdir(diretorio):
+            if filename.endswith('.csv'):
+                filepath = os.path.join(diretorio, filename)
+                df = pd.read_csv(filepath, sep=";")
+                dfs.append(df)
+        return pd.concat(dfs, ignore_index=True)
+    except Exception as e:
+        logging.info(f"Ocorreu um erro em 'combinando_arquivos': {str(e)}")
+        return None 
+    
 def remover_quebras_de_linha(df, caracteres_a_remover):
-    for coluna in df.columns:
-        if df[coluna].dtype == 'object':
-            df[coluna] = df[coluna].str.replace(caracteres_a_remover, '')
+    try:
+        for coluna in df.columns:
+            if df[coluna].dtype == 'object':
+                df[coluna] = df[coluna].str.replace(caracteres_a_remover, '')
+    except Exception as e:
+            logging.info(f"Ocorreu um erro em 'remover_quebras_de_linha': {str(e)}")
+            return None
+    return df
+
 
 def padronizar_data(data_original):
     try:
@@ -92,28 +118,28 @@ def upload_arquivo(data_atual, df_pessoa, df_planetas, df_filme, path):
     tornou um ponto de integração entre o contêiner do Airflow e o meu 
     sistema local, facilitando o gerenciamento e a visualização dos dados.
     '''
-
     base_path = '/opt/airflow/lake'
+    try:
+        pessoa_folder_path = os.path.join(base_path, path, 'people')
+        planetas_folder_path = os.path.join(base_path, path, 'planets')
+        filme_folder_path = os.path.join(base_path, path, 'films')
 
-    pessoa_folder_path = os.path.join(base_path, path, 'people')
-    planetas_folder_path = os.path.join(base_path, path, 'planets')
-    filme_folder_path = os.path.join(base_path, path, 'films')
+        for folder_path in [pessoa_folder_path, planetas_folder_path, filme_folder_path]:
+            os.makedirs(folder_path, exist_ok=True)
 
-    for folder_path in [pessoa_folder_path, planetas_folder_path, filme_folder_path]:
-        os.makedirs(folder_path, exist_ok=True)
+        pessoa_csv_path = os.path.join(pessoa_folder_path, f'pessoas_{data_atual}.csv')
+        planetas_csv_path = os.path.join(planetas_folder_path, f'planetas_{data_atual}.csv')
+        filme_csv_path = os.path.join(filme_folder_path, f'filmes_{data_atual}.csv')
 
-    pessoa_csv_path = os.path.join(pessoa_folder_path, f'pessoas_{data_atual}_1.csv')
-    planetas_csv_path = os.path.join(planetas_folder_path, f'planetas_{data_atual}_1.csv')
-    filme_csv_path = os.path.join(filme_folder_path, f'filmes_{data_atual}_1.csv')
+        df_pessoa.to_csv(pessoa_csv_path, index=False, sep=';')
+        df_planetas.to_csv(planetas_csv_path, index=False, sep=';')
+        df_filme.to_csv(filme_csv_path, index=False, sep=';')
+        logging.info("Arquivos salvos com sucesso.")
 
-    logging.info(f'Caminho de pessoas: {pessoa_csv_path}')
-    logging.info(f'Caminho de planetas: {planetas_csv_path}')
-    logging.info(f'Caminho de filmes: {filme_csv_path}')
-
-    df_pessoa.to_csv(pessoa_csv_path, index=False, sep=';')
-    df_planetas.to_csv(planetas_csv_path, index=False, sep=';')
-    df_filme.to_csv(filme_csv_path, index=False, sep=';')
-    logging.info("Arquivos salvos com sucesso.")
+    except OSError as e:
+        logging.error(f"Erro ao criar diretórios: {str(e)}")
+    except Exception as e:
+        logging.error(f"Erro inesperado: {str(e)}")
 
 
 def renomear_colunas():
